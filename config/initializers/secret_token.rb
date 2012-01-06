f98114e598559924a42f12c366f26ee6a5b198b4
Yasuhiro Usutani <y_usutani@me.com>
2012-01-06 15:21:46 +0900

    最初のコミット

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geo::Application.config.secret_token = '692346046efcb7c3ef08b88f3d33193f09261b7d19638fa7ea40992468f65b8332b78331acb19cd19cd9ade599a67b5a4ccf09000431472a29053264766fabc2'
