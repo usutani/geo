b33ad435351b8c8c8f648bcea6325926c6a4c2a5
Yasuhiro Usutani <y_usutani@me.com>
2012-01-06 17:10:56 +0900

    近接するマーカーを表示

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,4 +2,27 @@ class Location < ActiveRecord::Base
   attr_accessible :address, :latitude, :longitude
   geocoded_by :address
   after_validation :geocode, :if => :address_changed?
+
+  def map_string
+   str =  "http://maps.google.com/maps/api/staticmap"
+   str += "?size=450x300"
+   str += "&sensor=false"
+   str += "&zoom=10"
+   str += "&markers="
+   str += marker_string
+
+   for location in nearbys(30)
+     str += "|"
+     str += location.marker_string
+   end
+
+   return str
+  end
+
+  def marker_string
+    str = latitude.to_s
+    str += ","
+    str += longitude.to_s
+    return str
+  end
 end
